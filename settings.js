fadbad70d44008da4bf0815d6ed3ee26f12951e6
Bigun Shashra <liyomd24@gmail.com>
2025-06-30 14:46:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=xz0SDRob#ptNkGfN778sTMDMjdfvE_3zs1TGOtlqo0L7iwjrRqSg' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=prVy2DpC#btMEqyCkrJmzklQlRve2YWZH9FU2TyrKfbYtzjgMnzY' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
